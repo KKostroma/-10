fc0bfa3bb732e13c63b2e87f9c931d55cc810fab
Ekaterina Kostromitskaia <kostroma.mi@mail.ru>
2021-12-04 19:52:37 +0300

    Финализация

--- Задание 10/Program.cs
+++ Задание 10/Program.cs
@@ -10,6 +10,36 @@ namespace Задание_10
     {
         static void Main(string[] args)
         {
+            Corner corner = new Corner(60,2,30);
+            Console.WriteLine(corner.gradus);
+            Console.WriteLine(corner.min);
+            Console.WriteLine(corner.sec);
+            corner.Radians();
+            Console.ReadKey();
+        }
+    }
+    class Corner
+    {
+        public double gradus;
+        public int min;
+        public int sec;
+
+
+        public Corner(double gradus, int min, int sec)
+        {
+            this.gradus = gradus;
+            this.min = min;
+            this.sec = sec;
+            if (min <= 0 || sec < 0)
+            {
+                Console.WriteLine("Ошибка!");
+            }
+        }
+
+        public void Radians()
+        {
+            double r =  (Math.PI / 180) * gradus;
+            Console.WriteLine("Радиан угла равен {0}", r);
         }
     }
 }
